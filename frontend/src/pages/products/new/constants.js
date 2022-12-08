ed6f908a3bb6c405f1517ff2c46e7579ab91b8b3
Harun Jeylan <harunjeylanwako@gmail.com>
2022-12-08 16:45:48 +0000

    Create constants.js

---
+++ frontend/src/pages/products/new/constants.js
@@ -0,0 +1,31 @@
+export const constants = {
+  categories: [
+    { name: "Men's Clothing", value: "Men" },
+    { name: "Womens's Clothing", value: "Wom" },
+    { name: "Kid's Clothing", value: "Kid" },
+  ],
+  vendors: [
+    { name: "Men's Clothing", value: "Men" },
+    { name: "Womens's Clothing", value: "Wom" },
+    { name: "Kid's Clothing", value: "Kid" },
+  ],
+  tags: [
+    { name: "Men's Clothing", value: "Men" },
+    { name: "Womens's Clothing", value: "Wom" },
+    { name: "Kid's Clothing", value: "Kid" },
+  ],
+  variants: [
+    {
+      name: "Color",
+      type: "color",
+      value: "",
+      options: ["red", "yellow", "green", "blue"],
+    },
+    {
+      name: "Size",
+      type: "string",
+      value: "",
+      options: ["large", "medium", "small", "xsmall"],
+    },
+  ],
+};
