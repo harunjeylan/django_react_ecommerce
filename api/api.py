540d5ade4c1d1aab5b4982e84884a1362ba72e85
Jarun Jeylan <harunjeylanwako@gmail.com>
2022-12-11 17:30:21 +0300

    setup Jwt update

--- api/api.py
+++ api/api.py
@@ -1,6 +1,36 @@
+from django.http import JsonResponse
 from api.models import Order, Product
 from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 from api.serializer import ProductSerializer, OrderSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        "/api/token",
+        "api/token/refresh"
+    ]
+
+    return Response(routes)
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,22 @@
+from django.urls import path
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from api.api import OrderViewSet, ProductViewSet,MyTokenObtainPairView
+from api import api
 
-from api.api import OrderViewSet, ProductViewSet
 
+
+urlpatterns = [
+    path("", api.getRoutes, name="getRoutes"),
+
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
 router = routers.DefaultRouter()
 router.register('api/products', ProductViewSet, 'products')
 router.register('api/orders', OrderViewSet, 'orders')
 
-urlpatterns = router.urls
+urlpatterns += router.urls
