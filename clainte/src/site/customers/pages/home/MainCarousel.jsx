54226ec8255830d3cbc66eb0e859592520dbd88b
Harun Jeylan <harunjeylanwako@gmail.com>
2023-05-09 01:44:45 +0300

    removed import 'react-responsive-carousel/lib/styles/carousel.min.css'

--- clainte/src/site/customers/pages/home/MainCarousel.jsx
+++ clainte/src/site/customers/pages/home/MainCarousel.jsx
@@ -1,5 +1,4 @@
 import { Box, Button, Typography } from '@mui/material'
-import 'react-responsive-carousel/lib/styles/carousel.min.css'
 import { useNavigate } from 'react-router-dom'
 import { useTheme } from '@emotion/react'
 import { Swiper, SwiperSlide } from 'swiper/react'
